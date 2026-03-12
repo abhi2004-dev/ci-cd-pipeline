75036e4082458cb1f276a9332f4e027c38778f18
Abhi R Sooryavamshi <abhisooryavamshi99@gmail.com>
2026-03-12 15:40:33 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>ASHU ABHI</h2>
+      <h2>ASHU IS MY MALKIN</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
